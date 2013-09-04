167793caa0a7b05b88b61bdb97b8c4af06f250fd
jvbrink <j.v.d.brink@fys.uio.no>
2013-09-04 17:10:47 +0200

    Almost finished! =D

--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 from numpy import *
 
 class Problem:
+	"""Class holding physical parameters"""
 	def __init__(self, v0=0.0, m=90., C_D=1.4, rho=1.0, A=0.7, g=9.81, C_D_p=1.8, A_p=44):
 		self.v0 = v0
 		self.m = m
@@ -12,6 +13,7 @@ class Problem:
 		self.A_p = A_p
 
 	def define_command_line_options(self, parser=None):
+		"""Method for reading parameters from cml"""
 		if parser is None:
 			import argparse
 			parser = argparse.ArgumentParser()
@@ -61,21 +63,10 @@ class Problem:
 	def init_from_command_line(self, args):
 		self.v0, self.m, self.C_D, self.rho, self.A, self.g = args.v0, args.m, args.C_D, args.rho, args.A, args.g
 
-	def exact_solution(self, t):
-		b, c = self.b, self.v0
-		return b*t + c	
-       
-	def source_term(self, t, b=1.0):
-		self.b = b
-		c = self.v0
-		a = self.C_D * self.rho * self.A / 2.
-		m, g = self.m, self.g
-
-		return a * abs(b*t+c) * (b*t+c) + m * (b - g)
 
 class Solver:
-	def __init__(self, problem, dt=10e-4, T=10.0):
-		
+	"""Class for solving the ODE"""
+	def __init__(self, problem, dt=1e-3, T=120.0, b=1.0, mms=0, t_p=60., parachute=True):
 		N = ceil(T/float(dt)) + 1
 		v = zeros(N)
 		t = linspace(0, T, N)
@@ -83,14 +74,25 @@ class Solver:
 		self.problem = problem
 		self.k = 0				     # step number
 		self.dt = dt 				 # time step (s)
+		self.b = b
+		self.parachute = parachute
+		self.t_p = t_p
 		self.T = T 					 # end time (s)
-		self.N = int(ceil(T/dt)) + 1 # number of mesh points
-		self.v = zeros(self.N) 	     # velocity array
-		self.v[0] = problem.v0 	     # initial velocity (m/s)
-		self.t = linspace(0, T, self.N)	 # time array
-		self.f = problem.source_term
-	
+
+
+		if mms == 0:
+			# No source term
+			self.f = lambda x: 0
+		elif mms == 1:
+			# Source term solving discrete equation
+			self.f = self.source_term
+		elif mms == 2:
+			# Source term solving continuous ODE
+			self.f = self.cr_source_term
+		
+
 	def define_command_line_options(self, parser):
+		"""Method for reading parameters from cml"""
 		parser.add_argument(
 			'--dt', '--time_step', type=float,
 			default=self.dt, help='time step',
@@ -101,20 +103,60 @@ class Solver:
 			default=self.T, help='end time',
 			metavar='T')
 
+		parser.add_argument(
+			'--parachute', type=bool,
+			default=self.parachute, help='does the skydiver have a parachute?',
+			metavar='parachute')
+
+		parser.add_argument(
+			'--t_p', '--pull_time', type=float,
+			default=self.t_p, help='pull time',
+			metavar='t_p')
+
 		return parser
 
 	def init_from_command_line(self, args):
-		self.dt, self.T = args.dt, args.T
-		
+		self.dt, self.T, self.parachute, self.t_p = args.dt, args.T, args.parachute, args.t_p
+
+	def exact_solution(self, t):
+		"""Exact solution used for verification, 
+		defined through MMS"""
+		prob = self.problem
+		b, c = self.b, prob.v0
+		return b*t + c	
+       
+	def source_term(self, t):
+		"""Source term solving the discrete equation
+		exactly."""
+		prob = self.problem
+		b, c = self.b, prob.v0
+		a = prob.C_D * prob.rho * prob.A / 2.
+		m, g = prob.m, prob.g
+		dt = self.dt
+
+		return a * abs(b*(t+0.5*dt)+c) * (b*(t-0.5*dt)+c) + m * (b - g)
+
+	def cr_source_term(self, t):
+		"""Source term solving the continuous ODE,
+		used for verifying convergence rate."""
+
+		prob = self.problem
+		b, c = self.b, prob.v0
+		a = prob.C_D * prob.rho * prob.A / 2.
+		m, g = prob.m, prob.g
+		dt = self.dt
+
+		return a * abs(b*t+c) * (b*t+c) + m * (b - g)
 
 	def step(self):
+		"""Method for taking one step in time"""
 		v = self.v
 		k = self.k
 		f = self.f
 		dt = self.dt
 
 		prob = self.problem
-		a = prob.C_D * prob.rho * prob.A / 2.
+		a = self.C_D[k] * prob.rho * self.A[k] / 2.
 		m, g = prob.m, prob.g
 
 		v[k+1] = (v[k] + dt * (g + f((k+0.5)*dt)/m)) / (1. + a*dt*abs(v[k])/m)
@@ -122,53 +164,61 @@ class Solver:
 		self.k += 1
 		self.v = v
 
-	def solve(self, parachute=False, t_p=60.):
-		# set initial velocity
-		self.v[0] = self.problem.v0 # initial velocity (m/s)
+	def solve(self):
+		"""Method for solving the problem from t=0 to t=T"""
+		self.N = int(ceil(self.T/self.dt)) + 1	# number of mesh points
+		self.v = zeros(self.N) 	    			# velocity array
+		self.v[0] = self.problem.v0 	    	# initial velocity (m/s)
+		self.t = linspace(0, self.T, self.N)	# time array
 		C_D, A = self.problem.C_D, self.problem.A
+		parachute, t_p = self.parachute, self.t_p
 
+		# Set initial velocity
+		self.v[0] = self.problem.v0 # initial velocity (m/s)
+
+		# Check if pull time is reasonable
 		if parachute:
-			# Number of free fall steps
-			P = int(ceil(t_p/self.dt))
-			
-			# Solve for free fall, t in (0, t_p]
-			for i in range(P):
-				self.step()
-			
-			# Deployment phase
-			D = int(ceil(5./self.dt)) # Number of deployment steps
-			dA = (self.problem.A_p - self.problem.A)/D
-			dC_D = (self.problem.C_D_p - self.problem.C_D)/D
+			if t_p > self.T:
+				import sys
+				print "Error: t_p > T, exiting program"
+				sys.exit()
 
-			for i in range(D):
-				self.problem.A += dA
-				self.problem.C_D += dC_D
-				self.step()
-
-			# Solve under canopy, t in (t_p, T]
-			for i in range(self.N-1-P-D):
-				self.step()
-		
+		# Define area and drag coefficient as a function of time
+		self.C_D = zeros(self.N)
+		self.A = zeros(self.N)
+		if parachute:
+			F = int(ceil(t_p/self.dt)) # Number of free fall steps
+			D = int(ceil(8./self.dt))  # Number of deployment steps
+			
+			self.A[:F] = self.problem.A
+			self.A[F:F+D] = linspace(self.problem.A, self.problem.A_p, D)[:]
+			self.A[F+D:] = self.problem.A_p
+			
+			self.C_D[:F] = self.problem.C_D
+			self.C_D[F:F+D] = linspace(self.problem.C_D, self.problem.C_D_p, D)[:]
+			self.C_D[F+D:] = self.problem.C_D_p
 		else:
-			# Solve problem for t in (0, T]
-			for i in range(self.N-1):
-				self.step()
+			self.C_D[:] = self.problem.C_D 
+			self.A[:] = self.problem.A
 
-		# Repack the parachute
-		self.problem.C_D, self.problem.A = C_D, A
+		# Solve problem for t in (0, T]
+		for i in range(self.N-1):
+			self.step()
 
 		return self.v, self.t
 
-	
-
 
 class Visualizer:
+	"""Class for visualizing results, and
+	calculating error"""
+
 	def __init__(self, problem, solver):
 		import matplotlib.pyplot as plt
 		self.plt = plt
 		self.problem, self.solver = problem, solver
 
-	def plot(self, include_exact=False):
+	def plot(self, include_exact=False, plot=None):
+		"""Plot velocity of skydiver vs time"""
 		solver, problem = self.solver, self.problem
 		plt = self.plt
 
@@ -176,26 +226,31 @@ class Visualizer:
 		plt.xlabel(r'$t$ (s)', fontsize=16)
 		plt.ylabel(r'$v$ (m/s)', fontsize=16)
 		plt.title(r'Fallrate of a skydiver', fontsize=16)
-		#plt.axis([solver.t[0], solver.t[-1], 0, 1.1*max(solver.v)])
 		plt.grid()
+		if plot:
+			plt.savefig(plot + '.pdf')
 		plt.show()
 
-	def plot_error(self):
+	def plot_error(self, plot=None):
+		"""Plot error found using MMS"""
 		solver, problem = self.solver, self.problem
 		plt = self.plt
 
-		error = abs(solver.v - problem.exact_solution(solver.t))
+		error = abs(solver.v - solver.exact_solution(solver.t))
 		print max(error)
 		
 		plt.plot(solver.t, log10(error))
 		plt.xlabel(r'$t$ (s)', fontsize=16)
 		plt.ylabel(r'$\log_{10}(u-u_e)$', fontsize=16)
 		plt.title(r'Error using MMS', fontsize=16)
-		plt.legend([r'b=%g, c=%g' % (problem.b, problem.v0)], 4)
+		plt.legend([r'b=%g, c=%g' % (solver.b, problem.v0)], 4)
 		plt.grid()
+		if plot:
+			plt.savefig(plot + '.pdf')
 		plt.show()
 
-	def plot_height(self, z0=3000.):
+	def plot_height(self, z0=3000., plot=None):
+		"""Plot height above ground vs time"""
 		problem, solver = self.problem, self.solver
 		plt = self.plt
 		N, v, dt, t = solver.N, solver.v, solver.dt, solver.t
@@ -210,15 +265,18 @@ class Visualizer:
 		plt.ylabel(r'$z$ (m)', fontsize=16)
 		plt.title(r'Height above ground level')
 		plt.grid()
+		if plot:
+			plt.savefig(plot + '.pdf')
 		plt.show()
 
 	def plot_forces(self, plot=None):
+		"""Plot gravity and drag force vs time"""
 		solver, problem = self.solver, self.problem
 		plt = self.plt
 		
 		t = solver.t 
 		g = zeros(len(t)); g[:] = problem.m*problem.g
-		C_D, rho, A, v = problem.C_D, problem.rho, problem.A, solver.v
+		C_D, rho, A, v = solver.C_D, problem.rho, solver.A, solver.v
 		F_d = 0.5*C_D*rho*A*abs(v)*v
 
 		plt.plot(t, g/1000.)
@@ -231,66 +289,124 @@ class Visualizer:
 		if plot:
 			plt.savefig(plot + '.pdf')
 		plt.show()
-
 		
-	def plot_exact(self):
+	def plot_exact(self, plot=None):
+		"""Plot the exact solution used in MMS"""
 		solver, problem = self.solver, self.problem
 		plt = self.plt
 
 		plt.plot(solver.t, problem.exact_solution(solver.t))
+		plt.xlabel(r'$t$ (s)', fontsize=16)
+		plt.ylabel(r'$v$ (t)', fontsize=16)
+		plt.title(r'Exact solution, as demanded for MMS', fontsize=16)
+		plt.grid()
+		if plot:
+			plt.savefig(plot + '.pdf')
 		plt.show()
 
 def test_mms():
+	"""Function for testing the numerical scheme
+	through MMS, should reproduce linear solution
+	to machine precision."""
 	import nose.tools as nt
-	problem = Problem()
-	solver = Solver(problem)
-
+	problem = Problem(v0=3.9)
+	solver = Solver(problem, dt=1e-4, b=2.3, mms=1, T=10., parachute=False)
+	print "\nTesting for a linear exact solution using MMS: "
+	print "v(t)=bt + c, b=%g, c=%g, dt=%.2e" % (solver.b, problem.v0, solver.dt)
 	solver.solve()
-	diff = abs(solver.v - problem.exact_solution(solver.t)).max()
-	delta = 1E-14
-	nt.assert_almost_equal(diff, 0, delta=delta, msg='Testing MMS, v(t)=bt+c, delta=%e' % delta)
 
+	# Use L-infty norm
+	diff = abs(solver.v - solver.exact_solution(solver.t)).max()
+	print "Max error: %g" % diff
+	delta = 1E-12
+	nt.assert_almost_equal(diff, 0, delta=delta, msg='Testing MMS, v(t)=bt+c, delta=%.2e' % delta)
+
+	# Print plot of error
+	#vis = Visualizer(problem, solver)
+	#vis.plot_error(plot="errorplot")
+
+def test_convergence():
+	"""Function for testing the convergence rate
+	of the numerical scheme using MMS, the CR should
+	be close to 2, as a CN difference is used."""
+	import nose.tools as nt
+	b = 2.3;  c = 3.9
+	print "\nTesting Convergence Rate of Solver: "
+	print "v(t) = bt + c, b=%g, c=%g" % (b, c)
+	problem = Problem(v0=c)
+	solver = Solver(problem, mms=2, b=b, dt=1., T=10., parachute=False)
+	v, t = solver.solve()
+
+	# Previous time step
+	dtp = 1.
+	# Error for previous time step, using L2-norm
+	e = (v - solver.exact_solution(solver.t))**2
+	error = abs(solver.v - solver.exact_solution(solver.t))
+
+	Ep = sqrt(sum(e*dtp))
+	
+	for dt in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]:
+		solver = Solver(problem, mms=2, b=b, dt=dt, T=10., parachute=False)
+		v, t = solver.solve()
+
+		# Calculate L2-norm for new dt
+		e = (v - solver.exact_solution(solver.t))**2
+		E = sqrt(sum(e*dt))
+
+		# Compute convergence rate
+		r = (log(E) - log(Ep))/(log(dt) - log(dtp))
+
+		# Update values
+		Ep = E
+		dtp = dt
+
+		# Assert using nosetools
+		nt.assert_almost_equal(r, 2, places=1)
+
+		print "dt = %6.2e, E = %8e, r = %8g" % (dt, E, r)
 
 # Example of use and testing
 if __name__ == '__main__':
+	# Set up problem and solver
 	problem = Problem()
-	solver = Solver(problem, dt=1e-4)
+	solver = Solver(problem)
 	
+	# Read arguments from command-line
 	parser = problem.define_command_line_options()
 	parser = solver.define_command_line_options(parser)
 	args = parser.parse_args()
 	problem.init_from_command_line(args)
 	solver.init_from_command_line(args)
 	
+	# Set up visualizer
 	vis = Visualizer(problem, solver)
+	
+	# Solve problem
 	solver.solve()
+
+	# Plot results
 	vis.plot()
-	vis.plot_error()
-	
-
-	'''
-	Ep = 1
-	dtp = 1
-	for dt in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]:
-		solver = Solver(problem, dt=dt)
-		v, t = solver.solve()
-
-		e = (v - problem.exact_solution(solver.t))**2
-		E = sqrt(sum(e*dt))
-
-		r = (log(E) - log(Ep))/(log(dt) - log(dtp))
-
-		Ep = E
-		dtp = dt
-
-		print "dt = %g         E = %g      r = %g" % (dt, E, r)
-	'''
-	
-	'''
-	problem = Problem()
-	solver = Solver(problem, dt=1e-4, T=180)
-	vis = Visualizer(problem, solver)
-	solver.solve(parachute=True)
 	vis.plot_height()
-	'''
-	
+	vis.plot_forces()
+
+
+"""
+$>>> nosetests -s project1.py
+
+Testing for a linear exact solution using MMS: 
+v(t)=bt + c, b=2.3, c=3.9, dt=1.00e-04
+Max error: 2.59348e-13
+.
+Testing Convergence Rate of Solver: 
+v(t) = bt + c, b=2.3, c=3.9
+dt = 1.00e-01, E = 7.624395e-04, r =  2.01564
+dt = 1.00e-02, E = 7.599604e-06, r =  2.00141
+dt = 1.00e-03, E = 7.597157e-08, r =  2.00014
+dt = 1.00e-04, E = 7.598826e-10, r =   1.9999
+dt = 1.00e-05, E = 7.535791e-12, r =  2.00362
+.
+----------------------------------------------------------------------
+Ran 2 tests in 10.740s
+
+OK
+"""
